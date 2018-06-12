f35ee3237179e443f9d623eda62320f505fea133
Henry Heguang Miao <heguang.miao@coroma.com.au>
2018-06-12 17:45:08 +1000

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Henry Miao on 12/6/18.
+//  Copyright © 2018 Henry Miao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
