671143158cc2e04047751dc709f08d09fcbbba6c
Henry Heguang Miao <heguang.miao@coroma.com.au>
2018-06-13 21:35:16 +1000

    Fixed tests

--- ImageFeedTests/ResponseParserTests.swift
+++ ImageFeedTests/ResponseParserTests.swift
@@ -11,8 +11,11 @@ import XCTest
 
 class ResponseParserTests: XCTestCase {
     
+    var dummyParser: BaseResponseParser<Int>!
+    
     override func setUp() {
         super.setUp()
+        dummyParser = BaseResponseParser<Int>.init(data: nil, response: nil, error: nil)
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -23,14 +26,16 @@ class ResponseParserTests: XCTestCase {
     
     func testCheckResponseWithError() {
         let error = NSError(domain: "test", code: 123, userInfo: nil)
-        XCTAssertThrowsError(try ResponseParser.checkResponse(data: nil, response: nil, error: error)) { e in
+        dummyParser.error = error
+        XCTAssertThrowsError(try dummyParser.checkResponse()) { e in
             XCTAssertEqual(e as NSError, error, "Error should be rethrown")
         }
     }
     
     func testCheckResponseWithEmptyData() {
         do {
-            try ResponseParser.checkResponse(data: nil, response: HTTPURLResponse(url: Constants.Networking.feedURL, statusCode: 200, httpVersion: "1.1", headerFields: nil), error: nil)
+            dummyParser.response = HTTPURLResponse(url: Constants.Networking.feedURL, statusCode: 200, httpVersion: "1.1", headerFields: nil)
+            try dummyParser.checkResponse()
         } catch {
             XCTAssertFalse(true, "Empty data should pass the response check")
         }
@@ -38,6 +43,7 @@ class ResponseParserTests: XCTestCase {
     
     func testCheckResponseWithFailureStatusCode() {
         let response = HTTPURLResponse(url: Constants.Networking.feedURL, statusCode: 404, httpVersion: "1.1", headerFields: nil)
-        XCTAssertThrowsError(try ResponseParser.checkResponse(data: nil, response: response, error: nil))
+        dummyParser.response = response
+        XCTAssertThrowsError(try dummyParser.checkResponse())
     }
 }
